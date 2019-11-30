652bac44d5d92b3606b7fffb42a3b75a9fcaa53d
aapalo <aapalo@users.noreply.github.com>
2019-11-30 17:58:47 +0200

    Day 0

---
+++ 0/koodi.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+#from collections import Counter
+#import re
+#import os
+import time
+#from collections import defaultdict
+#from collections import deque
+'''     #######     '''
+
+date = 0
+dev = 0 # extra prints
+part = 1 # 1,2, or 3 for both
+samp = 0 # 0 or 1
+
+'''     #######     '''
+
+
+def day(te):
+
+    return 0
+
+'''     #######     '''
+
+time0 = time.time()
+
+if samp == 1:
+    filename = "/sample.txt"
+else:
+    filename = "/input.txt"
+
+try:
+    with open(str(date) + filename,"r") as f:
+        t = f.readlines()
+except FileNotFoundError:
+    with open("." + filename,"r") as f:
+        t = f.readlines()
+
+t = [(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
+rows = len(t)
+cols = len(t[0])
+print(rows,cols)
+if part == 1:
+    print("Part 1: ", day(t))
+elif part == 2:
+    print("Part 2: ", day(t))
+elif part == 3:
+    #run both
+    print("Part 1: ", day(t))
+    print("Part 2: ", day(t))
+
+tdif = time.time() - time0
+print("Elapsed time: {:.4f} s".format(tdif))
