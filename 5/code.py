12f908c597df86dc311effa4f9bfb69238717e30
Aake Paloniemi <aake.paloniemi@aalto.fi>
2019-12-05 16:19:12 +0200

    Day5a

--- 5/code.py
+++ 5/code.py
@@ -7,7 +7,7 @@ import time
 date = 5
 dev = 0 # extra prints
 part = 1 # 1,2, or 3 for both
-samp = 1 # 0 or 1
+samp = 0 # 0 or 1
 
 '''     #######     '''
 
@@ -17,29 +17,53 @@ def day(te):
     for i in te[0].split(","):
         a.append(int(i))
     idx = 0
-    if not samp:
-        a[1] = 12
-        a[2] = 2
+    puzInput = 1
+    puzOutputs = set()
+    print(len(a))
+    print(a)
     while idx < len(a):
-        print(a[idx])
-        instr = a[idx]
+        #print(a[idx])
+        i = str(a[idx])
+        while len(i) < 5:
+            i = '0' + i
+        instr = int(i[3:])
+        mode1 = int(i[2])
+        mode2 = int(i[1])
+        mode3 = int(i[0])
+        if dev:
+            print(a)
+            print(a[idx:(idx+4)], instr)
+        if 0:
+            print(i, mode1, mode2, mode3, instr)
         if instr == 1:
             # add
-            a[a[idx+3]] = a[a[idx+1]] + a[a[idx+2]]
+            p1 = a[idx+1] if mode1 else a[a[idx+1]]
+            p2 = a[idx+2] if mode2 else a[a[idx+2]]
+            a[a[idx+3]] = p1 + p2
             idx += 4
         elif instr == 2:
-            a[a[idx+3]] = a[a[idx+1]] * a[a[idx+2]]
+            p1 = a[idx+1] if mode1 else a[a[idx+1]]
+            p2 = a[idx+2] if mode2 else a[a[idx+2]]
+            a[a[idx+3]] = p1 * p2
             idx += 4
         elif instr == 3:
-            a[a[idx+3]] = a[a[idx+1]] * a[a[idx+2]]
-            idx += 4
+            a[a[idx+1]] = puzInput
+            idx += 2
         elif instr == 4:
-            a[a[idx+3]] = a[a[idx+1]] * a[a[idx+2]]
-            idx += 4
+            puzOut = a[a[idx+1]] if mode1 else a[a[idx+1]]
+            if puzOut:
+                puzOutputs.add(puzOut)
+            print("Output: ", puzOut)
+            idx += 2
         elif instr == 99:
+            print("Program done.")
             break
-        idx += 1
-    return a[0]
+        else:
+            print("Error, unknown instruction: ", instr)
+            break
+    if dev:
+        print(a)
+    return puzOutputs
 
 def day2(te):
     b = []
@@ -71,7 +95,6 @@ except FileNotFoundError:
         t = f.readlines()
 
 t = [(x.strip().replace('<->','').replace('  ',' ')) for x in t]
-
 if part == 1:
     print("Part 1: ", day(t))
 elif part == 2:
